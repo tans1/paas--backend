dbf289fca519eca7c6c03ee0be15bdf858ab6836
Nahom Amare <nahomamare52@gmail.com>
2025-04-02 21:43:59 +0300

    add bulders constant for vue

---
+++ src/core/frame-works/vue/bundler.constants.ts
@@ -0,0 +1,28 @@
+export const BundlerMap = {
+ 
+  webpack: {
+    defaultBuildLocation : 'build',
+    dependancy : 'webpack',
+  },
+  vite: {
+    defaultBuildLocation : 'dist',
+    dependancy : 'vite',
+  },
+  rollup: {
+    defaultBuildLocation : 'dist',
+    dependancy : 'rollup',
+  },
+  parcel: {
+    defaultBuildLocation : 'dist',
+    dependancy : 'parcel',
+  },
+  esbuild: {
+    defaultBuildLocation: 'dist',
+    dependancy : 'esbuild',
+  },
+  fusebox: {
+    defaultBuildLocation: 'dist',
+    dependancy : 'fuse-box',
+  },
+  
+};
