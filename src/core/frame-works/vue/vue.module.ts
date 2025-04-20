f0f95815a988fa054fbf9cd5d819494ac0b13f63
Nahom Amare <nahomamare52@gmail.com>
2025-04-20 22:44:36 +0300

    Add dockerignore file for vue application

--- src/core/frame-works/vue/vue.module.ts
+++ src/core/frame-works/vue/vue.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { VueProjectScannerService } from './vue-project-scanner/vue-project-scanner.service';
 import { VueProjectService } from './vue-project-service';
-import { VueDockerfileService } from './vue-dockerfile/vue-dockerfile.service';
+import { VueDockerfileService } from './vue-docker-config/vue-dockerfile.service';
 import { AlsModule } from '@/utils/als/als.module';
+import { VueDockerIgnoreFileService } from './vue-docker-config/vue-dockerignorefile.service';
 
 @Module({
   providers: [
     VueProjectScannerService,
     VueProjectService,
     VueDockerfileService,
+    VueDockerIgnoreFileService
   ],
   imports: [AlsModule],
 })
