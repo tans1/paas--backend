7889e0c71477b5428bcd86e0340cfdb712655a38
Nahom Amare <nahomamare52@gmail.com>
2025-04-20 22:46:02 +0300

    bug fix: password missing after google signin

--- src/resources/auth/login/login.service.ts
+++ src/resources/auth/login/login.service.ts
@@ -11,9 +11,16 @@ export class LoginService {
   ) {}
   async logIn(email, pass) {
     const user = await this.usersService.findOneBy(email);
-    if (!(await bcrypt.compare(pass, user?.password))) {
+
+    if (!user || !user.password) {
       throw new UnauthorizedException();
     }
+
+    const isPasswordValid = await bcrypt.compare(pass, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException();
+    }
+
     const payload = { sub: user.id, email: user.email, role: user.role };
     return {
       access_token: await this.jwtService.signAsync(payload),
