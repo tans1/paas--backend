6508412c7410bc6680e18e2da5762917e0d03c5c
Nahom Amare <nahomamare52@gmail.com>
2025-04-20 22:43:49 +0300

    Add dockerignore file for react

--- src/core/frame-works/react/react.module.ts
+++ src/core/frame-works/react/react.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ReactProjectScannerService } from './react-project-scanner/react-project-scanner.service';
 import { ReactProjectService } from './react-project-service';
-import { ReactDockerfileService } from './react-dockerfile/react-dockerfile.service';
+import { ReactDockerfileService } from './react-docker-config/react-dockerfile.service';
 import { AlsModule } from '@/utils/als/als.module';
+import { ReactDockerIgnoreFileService } from './react-docker-config/react-dockerignorefile.service';
 
 @Module({
   providers: [
     ReactProjectScannerService,
     ReactProjectService,
     ReactDockerfileService,
+    ReactDockerIgnoreFileService,
   ],
   imports: [AlsModule],
-  exports : [ReactProjectScannerService]
+  exports: [ReactProjectScannerService],
 })
 export class ReactModule {}
